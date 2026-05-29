fcf6de8dab3853a1cc1614b6fd7a9b693ce53921
kspsingh9336o-ux <kspsingh9336o@gmail.com>
2026-05-29 16:39:45 +0530

    loop.js

---
+++ javascript/loop.js
@@ -0,0 +1,21 @@
+//using for loop
+var sum=0;
+for(let i=0;i<100;i++){
+    sum=sum+i;
+}
+console.log("sum :",sum);
+//using while loop
+let x=5;
+while(x>0){
+    console.log("kapil:");
+    x--;
+}
+// using do-while loop
+let xx=1;
+do{
+    console.log("kapilpatel:");
+     xx--;
+}
+while(xx>0);
+
+
