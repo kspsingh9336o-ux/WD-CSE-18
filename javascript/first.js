1029c4805f65b49c99710400e103340a431ca605
kspsingh9336o-ux <kspsingh9336o@gmail.com>
2026-05-29 16:34:51 +0530

    first.js

---
+++ javascript/first.js
@@ -0,0 +1,19 @@
+console.log("hello kapil patel!");
+console.log("how are you?");
+let fullName="kapil patel";
+ let age=25;
+a=null;
+//float avilue
+price =96.55;
+y=undefined;
+isFollow=true;
+console.log(fullName);
+console.log(age);
+const student={
+    name: "kapil",
+    roll: 15,
+    cgpa: 8.6,
+    ispass: true,
+};
+console.log(student["roll"]);
+// student["name"]="patel ji";
